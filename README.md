1dde1a44a76ba6abcfd6c7c7507f35cbf4ea6277
www-data <www-data@hhvm.trawg.com>
2016-03-29 06:15:10 +1100

    - Added support for OpenPGP-PHP - Added a profile option for WP users to store their PGP key - Added a WP plugin settings page to enable/disable encryption

--- README.md
+++ README.md
@@ -6,19 +6,31 @@ Note: if you're logged in into the site (including into /wp-admin) and enable th
 
 ### WARNINGS
 - this is a proof-of-concept; has not been thoroughly tested. May not be secure. 
+- it ONLY encrypts the output that comes directly from WordPress PHP components (e.g., the HTML returned from index.php). Other assets (e.g., JavaScript, CSS, images) are not encrypted. 
 - does NOT encrypt anything you send to the server - only encrypts the output from WordPress.
 
 # Requirements
 
-- Needs GPG installed
-- GPG keys for users that want encrypted pages need to be added to the GPG keyring (probably of the user that the web server runs as). 
+- Needs either GPG installed, or OpenPGP-PHP (which has other dependencies, like phpseclib)
+- If using GPG, the GPG keys for the WordPress users that want encrypted pages need to be added to the GPG keyring (probably of the user that the web server runs as). 
+- The wpgpg Chrome extension (https://github.com/trogau/wpgpg-extension) to do browser-side decryption of content.
 
-## Installation
+## Installation with GPG
 
 1. Install plugin as usual
-2. Edit config.php and put your GPG home dir (i.e., the directory which contains the GPG keyring you want to use - see below) in GPG_HOME_DIR, and your GPG binary location (GPG_BIN).
+2. Edit config.php and put your GPG home dir (i.e., the directory which contains the GPG keyring you want to use - see below) in GPG_HOME_DIR, and your GPG binary location (GPG_BIN). Set the PGP_ENCRYPT_MODE to 'gpg'.
 3. Set up the browser plugin so you can decrypt the content.
 4. Activate the plugin, if you dare. 
+5. Go to the wpgpg Options and check the 'GPG Encryption Enabled' checkbox. On 'save changes', the page should reload as a PGP-encrypted ASCII-armored chunk.
+
+## Installation with OpenPGP-PHP
+
+1. Install plugin as usual
+2. Edit config.php and set the PGP_ENCRYPT_MODE to 'openpgp-php'.
+3. Set up the browser plugin so you can decrypt the content.
+4. Activate the plugin, if you dare. 
+5. Log into WordPress and visit the profile page (Users->Your Profile, if you're logged in as usual). Paste your public key into the new field, 'GPG Public Key'.
+6. Go to the wpgpg Options and check the 'GPG Encryption Enabled' checkbox. On 'save changes', the page should reload as a PGP-encrypted ASCII-armored chunk.
 
 ## GPG home dir
 
@@ -26,4 +38,6 @@ The GPG home directory will depend on what user your webserver is running as. De
 
 ## TODO
 
-- Make it possible for users to add their GPG keys via some sort of config page
+- Figure out how to encrypt other content aside from the WordPress content (e.g., stylesheets, images, etc)
+- Look for other PHP/server side solutions for GPG.
+- [DONE] Make it possible for users to add their GPG keys via some sort of config page
--- wpgpg.php
+++ wpgpg.php
@@ -4,12 +4,18 @@ Plugin Name: wpgpg
 Plugin URI: http://trog.qgl.org/wpgpg
 Description: GPG/PGP encryption for WordPress output
 Author: trogau
-Version: 0.1.0
+Version: 0.1.1
 Author URI: http://trog.qgl.org
 License: GPLv2 or later
 */
 
 require_once("config.php");
+
+if (PGP_ENCRYPT_MODE == 'openpgp-php')
+{
+	require_once("openpgp-php/vendor/autoload.php");
+	require_once("openpgp-php/lib/helpers.php");
+}
 
 /**
  * Output Buffering
@@ -23,8 +29,8 @@ ob_start();
  * get_user()
  *
  * Get the current user information. If we're logged in, add an action for the page shutdown and set up the filter
- * to do the encryption via GPG. 
- * 
+ * to do the encryption via GPG.
+ *
  */
 function get_user()
 {
@@ -50,22 +56,40 @@ function get_user()
 		}, 0);
 
 
-
 		add_filter('final_output', function($output) {
 			global $current_user;
-			$cmd = GPG_BIN." --homedir ".GPG_HOME_DIR." --armor --batch -e -r '$current_user->user_email'";
-			$crypted = encrypt_command($cmd, $output);
-			return "<html><div id='decryptbtn'></div><br /><div id='crypted' style='white-space:pre;font-family:monospace;font-size:11px;width:500px;border:dotted 2px black; height:200px;overflow:auto'>$crypted</div></html>";
-		});
 
+			if (PGP_ENCRYPT_MODE == 'gpg')
+			{
+				$cmd = GPG_BIN." --homedir ".GPG_HOME_DIR." --armor --batch -e -r '$current_user->user_email'";
+				$crypted = encrypt_command($cmd, $output);
+			}
+			else if (PGP_ENCRYPT_MODE == 'openpgp-php')
+			{
+				// Get the stored user key
+				$public_key = get_user_meta($current_user->ID, 'gpgpublickey', true);
+				$key = OpenPGP_Message::parse(OpenPGP::unarmor(trim($public_key)));
+				$data = new OpenPGP_LiteralDataPacket($output, array('format' => 'u'));
+				$encrypted = OpenPGP_Crypt_Symmetric::encrypt($key, new OpenPGP_Message(array($data)));
+				$output = OpenPGP::enarmor($encrypted->to_bytes(), "PGP MESSAGE");
+				$crypted = wordwrap($output, 64, "\n", 1);
+			}
+			else
+			{
+				die("No encryption tool is set (see config.php:PGP_ENCRYPT_MODE)");
+			}
+
+			return "<html><body><div id='decryptbtn'></div><br /><div id='crypted' style='white-space:pre;font-family:monospace;font-size:14px;width:100%;border:dotted 1px black;overflow:auto'>$crypted</div></body></html>";
+		});
 	}
 }
 
 // Call the get_user() function once WordPress hits init - otherwise none of the user functionality has been loaded so we
 // can't retreive the email. 
 // FIXME: may be a better way to call this or a different trigger point that is more suitable. 
-add_action( 'init', 'get_user' );
-
+$options = get_option('wpgpg_options');
+if ($options['wpgpg_onoroff'] === "1")
+	add_action( 'init', 'get_user' );
 
 /**
  * encrypt_command
@@ -107,3 +131,127 @@ function encrypt_command ($gpg_command, $data)
 	}
 	return $return_value;
 }
+
+
+add_action( 'show_user_profile', 'gpgProfileField' );
+add_action( 'edit_user_profile', 'gpgProfileField' );
+
+function gpgProfileField($user)
+{
+?>
+	<h3><?php _e("Extra profile information", "blank"); ?></h3>
+	<table class="form-table">
+    <tr>
+      <th><label for="gpgpublickey"><?php _e("GPG Public Key"); ?></label></th>
+      <td>
+        <textarea name="gpgpublickey" id="gpgpublickey" class="regular-text"><?php echo esc_attr( get_the_author_meta( 'gpgpublickey', $user->ID ) ); ?></textarea><br />
+        <span class="description"><?php _e("Please enter your GPG Public Key."); ?></span>
+    </td>
+    </tr>
+  </table>
+<?php
+}
+
+add_action( 'personal_options_update', 'gpgSaveProfileField' );
+add_action( 'edit_user_profile_update', 'gpgSaveProfileField' );
+
+function gpgSaveProfileField( $user_id )
+{
+	$saved = false;
+	if ( current_user_can( 'edit_user', $user_id ) )
+	{
+		// Verify the key
+		if (isValidPublicKey($_POST['gpgpublickey']))
+		{
+			update_user_meta( $user_id, 'gpgpublickey', $_POST['gpgpublickey'] );
+			$saved = true;
+		}
+		else
+		{
+			// FIXME: better error handling
+			die("ERROR: invalid public key");
+		}
+	}
+	return true;
+}
+
+
+/**
+ * WordPress Options
+ *
+ * Taken from http://ottopress.com/2009/wordpress-settings-api-tutorial/ (referenced in official WordPress 
+ * docs here: https://codex.wordpress.org/Creating_Options_Pages
+ *
+ */
+
+add_action('admin_menu', 'plugin_admin_add_page');
+function plugin_admin_add_page()
+{
+	add_options_page('wpgpg Options', 'wpgpg Options', 'manage_options', 'wpgpg', 'wpgpg_options_page');
+}
+
+function wpgpg_options_page()
+{
+?>
+	<div>
+	<h2>wpgpg Options</h2>
+	WP GPG Options
+	<form action="options.php" method="post">
+	<?php settings_fields('wpgpg_options'); ?>
+	<?php do_settings_sections('wpgpg'); ?>
+
+	<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+	</form></div>
+
+<?php
+}
+
+add_action('admin_init', 'plugin_admin_init');
+
+function plugin_admin_init()
+{
+	register_setting( 'wpgpg_options', 'wpgpg_options', 'wpgpg_options_validate' );
+	add_settings_section('wpgpg_plugin_main', 'wpgpg Settings', 'wpgpg_section_text', 'wpgpg');
+	add_settings_field('wpgpg_enabled_checkbox', 'GPG Encryption Enabled', 'wpgpg_setting_checkbox', 'wpgpg', 'wpgpg_plugin_main');
+}
+
+function wpgpg_section_text()
+{
+	echo "";
+}
+
+
+function wpgpg_setting_checkbox()
+{
+	$options = get_option('wpgpg_options');
+
+	$html = '<input type="checkbox" id="wpgpg_onoroff" name="wpgpg_options[wpgpg_onoroff]" 
+		value="1"' . checked( 1, $options['wpgpg_onoroff'], false ) . '/>';
+	$html .= '<label for="wpgpg_onoroff">Check to enable wpgpg output encryption</label>';
+
+	echo $html;
+}
+
+function wpgpg_options_validate($input)
+{
+	if ($input['wpgpg_onoroff'] === NULL || $input['wpgpg_onoroff'] === "1")
+		return $input;
+}
+
+/**
+ * START Settings Link in Plugin List
+ * http://bavotasan.com/2009/a-settings-link-for-your-wordpress-plugins/
+ */
+function wpgpg_settings_link($links)
+{
+	$settings_link = "<a href='options-general.php?page=wpgpg'>Settings</a>";
+	array_unshift($links, $settings_link);
+	return $links;
+}
+
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", 'wpgpg_settings_link' );
+
+/**
+ * END Settings Link in Plugin List
+ */
